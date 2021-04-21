e1128a45466c58ab4ed000a75f147be07ad5137c
Jennifer Dallán Erazo Martínez <jennifer.erazo@yuxiglobal.com>
2021-04-21 16:43:55 -0500

    Solved T-3//added search command

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,19 +3,13 @@ describe('Search elements', () => {
         cy.visit('/');
     })
     it('Search for elements with multiple results', () => {
-        cy.fixture('index').then((index) => {
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress');
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.title).should('contain', 'dress');
         })
     })
     it('Search for elements with no results', () => {
-        cy.fixture('index').then((index) => {
-            cy.get(index.searchBox).type('qwerty');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('qwerty');
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.alert).should('contain', 'No results were found for your search');
         })
